0a1c73ec2f98f05a7706a6b25540196ba62ad475
Tevin Hamilton <40679667+TevinH14@users.noreply.github.com>
2025-08-29 16:27:28 -0500

    Update Game.kt
    updated properties in game file to hold the full game object form api call.

--- GameExplorerRemastered-Android/app/src/main/java/com/example/gameexplorerremastered_android/Features/model/Game.kt
+++ GameExplorerRemastered-Android/app/src/main/java/com/example/gameexplorerremastered_android/Features/model/Game.kt
@@ -1,6 +1,5 @@
 package com.example.gameexplorerremastered_android.Features.model
 
-import com.example.gameexplorerremastered_android.Features.AddedByStatus
 import com.example.gameexplorerremastered_android.Features.EsrbRating
 import com.example.gameexplorerremastered_android.Features.Platform
 import com.example.gameexplorerremastered_android.Features.Ratings
@@ -8,23 +7,36 @@ import com.google.gson.annotations.SerializedName
 
 
 data class Game(
-    val id: Int,
-    val slug: String,
-    val name: String,
-    val released: String,
-    val tba: Boolean,
-    @SerializedName("background_image") val backgroundImage: String,
-    val rating: Int,
-    @SerializedName("rating_top") val ratingTop: Int,
-    val ratings: Ratings,
-    val ratings_count: Int,
-    val added: Int,
-    val added_by_status: AddedByStatus,
-    val esrb_rating: EsrbRating,
-    val metacritic: Int,
-    val platforms: List<Platform>,
-    val playtime: Int,
-    val reviews_text_count: String,
-    val suggestions_count: Int,
-    val updated: String
+    val id: Int?,
+    val slug: String?,
+    val name: String?,
+    val released: String?,
+    val tba: Boolean?,
+    @SerializedName("background_image") val backgroundImage: String?,
+    val rating: Int?,
+    @SerializedName("rating_top") val ratingTop: Int?,
+    val ratings: Ratings?,
+    @SerializedName("ratings_count") val ratingsCount: Int?,
+    val reviews_text_count: String?,
+    val metacritic: Int?,
+    val playtime: Int?,
+    @SerializedName("suggestions_count") val suggestionsCount: Int?,
+    val updated: String?,
+    val userGame: String? = null,
+    val reviewsCount: Int? = null,
+
+    val platforms: List<Platform>?,
+    val added: Int?,
+    @SerializedName("added_by_status") val addedByStatus: AddedByStatus?,
+    val esrb_rating: EsrbRating?,
+
+    val saturatedColor: String? = null,
+    val dominantColor: String? = null,
+    val parentPlatforms: List<Platform>? = null,
+    val genres: List<Genre>? = null,
+    val stores: List<GameStore>? = null,
+    val clip: String? = null, // If clip is complex, create a separate data class
+    val tags: List<Tag>? = null,
+    @SerializedName("esrb_rating") val esrbRating: EsrbRating? = null,
+    val shortScreenshots: List<Screenshot>? = null
 )
