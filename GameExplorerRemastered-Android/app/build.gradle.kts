98a9ad20c3f096521a52a5d2b51649d92347dd14
Tevin Hamilton <40679667+TevinH14@users.noreply.github.com>
2025-10-25 14:02:09 -0500

    Update build.gradle.kts
    Added coroutines , OKHttp Logging, navigation compose, Gson Converter for Retrofit and Material 3 Design

--- GameExplorerRemastered-Android/app/build.gradle.kts
+++ GameExplorerRemastered-Android/app/build.gradle.kts
@@ -53,17 +53,31 @@ dependencies {
 
     // Retrofit
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
-// Gson converter for Retrofit
+   // Gson converter for Retrofit
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
 
+    // Coroutines
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 
+    // OkHttp logging (optional)
+    implementation ("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.11")
+
+    //navigation compose
+    implementation ("androidx.navigation:navigation-compose:2.8.0")
+
+    //core
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
+
+    //UI
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.ui)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
+
+    // material 3 design
     implementation(libs.androidx.material3)
 
     testImplementation(libs.junit)
