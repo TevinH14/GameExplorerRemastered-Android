af768b618477353560c1bded9947c4450a0288c1
Tevin Hamilton <40679667+TevinH14@users.noreply.github.com>
2026-01-28 17:49:13 -0600

    Update DrawerContent.kt
    updated Drawer header to use Screen class
    added comments to help developers understand whats going on
    updated divider to use horizontal divider
    updated package folder name

--- GameExplorerRemastered-Android/app/src/main/java/com/example/gameexplorerremastered_android/Navigation/DrawerContent.kt
+++ GameExplorerRemastered-Android/app/src/main/java/com/example/gameexplorerremastered_android/Navigation/DrawerContent.kt
@@ -1,4 +1,4 @@
-package com.example.gameexplorerremastered_android.Navigation
+package com.example.gameexplorerremastered_android.navigation
 
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.*
@@ -8,26 +8,32 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.gameexplorerremastered_android.ui.theme.GameExplorerRemasteredAndroidTheme
 
-
+// Composable that renders the drawer menu items.
+// The callback returns the route string when a destination is selected.
 @Composable
 fun DrawerContent(onDestinationClicked: (String) -> Unit) {
+    // Material3 drawer sheet container
+
     ModalDrawerSheet {
+        // Drawer Header Title
         Text(
             text = "Menu",
             style = MaterialTheme.typography.titleLarge,
             modifier = Modifier
                 .padding(16.dp)
         )
-        Divider()
+        HorizontalDivider()
+
+        // navigation items
         NavigationDrawerItem(
             label = { Text("Home") },
             selected = false,
-            onClick = { onDestinationClicked("home") }
+            onClick = { onDestinationClicked(Screen.Home.route) }
         )
         NavigationDrawerItem(
             label = { Text("Settings") },
             selected = false,
-            onClick = { onDestinationClicked("settings") }
+            onClick = { onDestinationClicked(Screen.Settings.route) }
         )
         NavigationDrawerItem(
             label = { Text("Profile") },
