388e5cc7cf96d835e64e8c26a71a3bc1afec7bb8
Tevin Hamilton <40679667+TevinH14@users.noreply.github.com>
2026-01-29 21:06:49 -0600

    Update RawgApiService.kt
    added dates and ordering to make more detail calls to get games

--- GameExplorerRemastered-Android/app/src/main/java/com/example/gameexplorerremastered_android/resourses/Services/Interfaces/RawgApiService.kt
+++ GameExplorerRemastered-Android/app/src/main/java/com/example/gameexplorerremastered_android/resourses/Services/Interfaces/RawgApiService.kt
@@ -9,7 +9,8 @@ interface RawgApiService {
     suspend fun getGames(
         @Query("page") page: Int = 1,
         @Query("page_size") pageSize: Int = 20,
+        @Query("dates") dates: String? = null, // 👈 ADD THIS
+        @Query("ordering") ordering: String? = null,
         @Query("key") apiKey: String = BuildConfig.RAWG_API_KEY
-
     ): RawgResponse
 }
