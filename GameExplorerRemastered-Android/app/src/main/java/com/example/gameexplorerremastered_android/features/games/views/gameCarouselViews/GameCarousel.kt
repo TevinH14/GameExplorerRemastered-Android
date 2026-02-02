fefe6a3d41dc9019c56cce737a55070571c2b3dd
Tevin Hamilton <40679667+TevinH14@users.noreply.github.com>
2026-02-02 15:15:44 -0600

    Update GameCarousel.kt
    Intergraded  GameEndIconLabel  into carousel to expand based displayed games

--- GameExplorerRemastered-Android/app/src/main/java/com/example/gameexplorerremastered_android/features/games/views/gameCarouselViews/GameCarousel.kt
+++ GameExplorerRemastered-Android/app/src/main/java/com/example/gameexplorerremastered_android/features/games/views/gameCarouselViews/GameCarousel.kt
@@ -4,18 +4,24 @@ import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ChevronRight
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
+import com.example.gameexplorerremastered_android.R
 import com.example.gameexplorerremastered_android.features.games.model.Game
+import com.example.gameexplorerremastered_android.resourses.Components.GameEndIconLabel
 import com.example.gameexplorerremastered_android.resourses.Components.GameSpace
 import com.example.gameexplorerremastered_android.resourses.Components.textComponent.GameFont
 import com.example.gameexplorerremastered_android.resourses.Components.textComponent.GameText
@@ -37,13 +43,26 @@ fun GameCarousel(
         // Top space
         GameSpace(height = 24)
 
-        //header text view
-        GameText(
-            text = header,
-            font = GameFont.Title,
-            alignment = Alignment.Start
-        )
+        Row(
+            modifier = Modifier
+            .fillMaxWidth()
+        ) {
+            //header text view
+            GameText(
+                text = header,
+                font = GameFont.Title,
+                alignment = Alignment.Start
+            )
+            Spacer(modifier = Modifier.weight(1f))
 
+            GameEndIconLabel(
+                title = stringResource(R.string.see_all_button),
+                font = GameFont.PrimaryButton,
+                endIcon = Icons.Filled.ChevronRight,
+                alignment = Alignment.End,
+                textWidth = 100.dp
+            )
+        }
         //space to separate header and list
         GameSpace(height = 16)
 
