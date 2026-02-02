be8e58a34e8614ed016ab2386c397164bbe23ed6
Tevin Hamilton <40679667+TevinH14@users.noreply.github.com>
2026-02-02 15:16:30 -0600

    Update SettingsView.kt
    updated settings to update topappbar and hold nav controller reference

--- GameExplorerRemastered-Android/app/src/main/java/com/example/gameexplorerremastered_android/Features/Settings/Views/SettingsView.kt
+++ GameExplorerRemastered-Android/app/src/main/java/com/example/gameexplorerremastered_android/Features/Settings/Views/SettingsView.kt
@@ -1,12 +1,15 @@
-package com.example.gameexplorerremastered_android.Features.Settings.Views
+package com.example.gameexplorerremastered_android.features.settings.views
 
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.navigation.NavController
 
 @Composable
-fun SettingsScreen() {
+fun SettingsScreen(navController: NavController, topBarTitle: MutableState<String>) {
     Surface {
+        topBarTitle.value = "Settings"
         Text("⚙️ Settings Screen")
     }
 }
