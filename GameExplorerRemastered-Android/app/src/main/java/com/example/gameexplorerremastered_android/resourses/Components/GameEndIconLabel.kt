58e11811b9ee6338d2a47ca0d657bff6f4896163
Tevin Hamilton <40679667+TevinH14@users.noreply.github.com>
2026-02-02 15:12:10 -0600

    Update GameEndIconLabel.kt
    fixed custom view display text and icons

--- GameExplorerRemastered-Android/app/src/main/java/com/example/gameexplorerremastered_android/resourses/Components/GameEndIconLabel.kt
+++ GameExplorerRemastered-Android/app/src/main/java/com/example/gameexplorerremastered_android/resourses/Components/GameEndIconLabel.kt
@@ -1,11 +1,19 @@
 package com.example.gameexplorerremastered_android.resourses.Components
 
+import android.graphics.drawable.Icon
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ChevronRight
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.example.gameexplorerremastered_android.resourses.Components.textComponent.GameFont
 import com.example.gameexplorerremastered_android.resourses.Components.textComponent.GameText
@@ -15,26 +23,46 @@ import com.example.gameexplorerremastered_android.resourses.Components.textCompo
 fun GameEndIconLabel(
     title: String,
     font: GameFont,
-    endIcon: String,
-    alignment: Alignment.Horizontal = Alignment.Start
+    endIcon: ImageVector? = null,
+    alignment: Alignment.Horizontal = Alignment.Start,
+    textWidth: Dp? = null
 ) {
     Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(horizontal = 16.dp),
         verticalAlignment = Alignment.CenterVertically,
-        horizontalArrangement = Arrangement.SpaceBetween
+        horizontalArrangement = Arrangement.End,
+        modifier = Modifier
+            .padding(horizontal = 16.dp)
+//            .fillMaxWidth()
     ) {
         GameText(
             text = title,
             font = font,
-            alignment = alignment
+            alignment = alignment,
+            modifier = Modifier
         )
-        Text(
-            text = endIcon,
-            style = font.textStyle(),
-            color = font.color(),
-            textAlign = TextAlign.End
+        GameSpace(width = 16)
+        if (endIcon != null) {
+        Icon(
+            imageVector = endIcon,
+            contentDescription = "Right Arrow",
+            tint = font.color(),
+            modifier = Modifier
+                .size(24.dp)
+//                .padding(horizontal = 8.dp)
         )
+        }
     }
+}
+
+// primary button preview
+@Preview(showBackground = true)
+@Composable
+fun GameTextPreview() {
+   GameEndIconLabel(
+       title = "See All",
+       font = GameFont.PrimaryButton,
+       endIcon = Icons.Filled.ChevronRight,
+       alignment = Alignment.End,
+       textWidth = 100.dp
+   )
 }
